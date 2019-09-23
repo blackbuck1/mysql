d072458c7881a694d71f79657424a99736d9c317
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2019-09-23 20:02:31 +0530

    Update cohorts.sql

--- cohorts.sql
+++ cohorts.sql
@@ -188,4 +188,103 @@ GROUP  BY 1,
           2, 
           3 
 ORDER  BY 2, 
-          1;
+          1 ASC;
+
+
+#Top 60 Customer (Quarter 15)
+SELECT rnk, 
+       c.* 
+FROM   (SELECT actual_customer_name, 
+               customer_classification, 
+               qtr_name, 
+               ROUND(SUM(gmv), 0)                 AS gmv, 
+               Count(order_id)                    AS orders, 
+               ROUND(SUM(demand_discount), 0)     AS demand_discount, 
+               ROUND(SUM(CASE 
+                           WHEN customer_type = 'MS Order' THEN 
+                           supply_incentive - other_cost 
+                           ELSE supply_incentive 
+                         END), 0)                 AS supply_incentive, 
+               ROUND(SUM(CASE 
+                           WHEN customer_type = 'MS Order' THEN gmv 
+                           - potential_cost 
+                           ELSE gmv - potential_cost + other_cost 
+                         END), 0)                 AS take_rate, 
+               ROUND(SUM(revenue - base_cost), 0) AS cont_margin 
+        FROM   zinka.table_name 
+        WHERE  actual_customer_name <> '' 
+               AND qtr <= 15 
+        GROUP  BY 1, 
+                  2, 
+                  3) c 
+       LEFT JOIN (SELECT RANK() 
+                           OVER( 
+                             partition BY qtr 
+                             ORDER BY gmv DESC) AS rnk, 
+                         actual_customer_name, 
+                         qtr, 
+                         gmv 
+                  FROM   (SELECT actual_customer_name, 
+                                 qtr, 
+                                 ROUND(SUM(gmv), 0) AS gmv 
+                          FROM   zinka.table_name 
+                          WHERE  actual_customer_name <> '' 
+                                 AND actual_customer_name NOT IN ( 
+                                     'Maple Logistics Pvt Limited', 
+                                     'SJA Shipping Pvt Ltd', 'GOSS' 
+                                                                 ) 
+                                 AND qtr = 15 
+                          GROUP  BY 1, 
+                                    2) a) d 
+              ON c.actual_customer_name = d.actual_customer_name 
+WHERE  rnk <= 60 
+ORDER  BY 1, 
+          4 ASC;
+
+#Top 60 Lanes
+SELECT d.rnk, 
+       c.* 
+FROM   (SELECT CONCAT(from_cluster, '-', to_cluster) AS cluster_lane, 
+               qtr_name, 
+               ROUND(SUM(gmv), 0)                    AS gmv, 
+               Count(order_id)                       AS orders, 
+               ROUND(SUM(demand_discount), 0)        AS demand_discount, 
+               ROUND(SUM(CASE 
+                           WHEN customer_type = 'MS Order' THEN 
+                           supply_incentive - other_cost 
+                           ELSE supply_incentive 
+                         END), 0)                    AS supply_incentive, 
+               ROUND(SUM(CASE 
+                           WHEN customer_type = 'MS Order' THEN gmv 
+                           - potential_cost 
+                           ELSE gmv - potential_cost + other_cost 
+                         END), 0)                    AS take_rate, 
+               ROUND(SUM(revenue - base_cost), 0)    AS cont_margin 
+        FROM   zinka.table_name 
+        WHERE  from_cluster <> '' 
+               AND to_cluster <> '' 
+               AND qtr <= 15 
+        GROUP  BY 1, 
+                  2) c 
+       LEFT JOIN (SELECT RANK() 
+                           OVER( 
+                             partition BY qtr 
+                             ORDER BY gmv DESC) AS rnk, 
+                         cluster_lane, 
+                         qtr, 
+                         gmv 
+                  FROM   (SELECT CONCAT(from_cluster, '-', to_cluster) AS 
+                                 cluster_lane, 
+                                 qtr, 
+                                 ROUND(SUM(gmv), 0)                    AS gmv 
+                          FROM   zinka.table_name 
+                          WHERE  from_cluster <> '' 
+                                 AND to_cluster <> '' 
+                                 AND qtr = 15 
+                          GROUP  BY 1, 
+                                    2) a) d 
+              ON c.cluster_lane = d.cluster_lane 
+WHERE  rnk <= 60 
+ORDER  BY 1, 
+          2, 
+          3 ASC;
