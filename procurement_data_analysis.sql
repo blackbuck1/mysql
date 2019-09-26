b7a606a28aaae055cd734fd029fcaf7289a41512
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2019-09-26 16:32:30 +0530

    Create procurement_data_analysis.sql

---
+++ procurement_data_analysis.sql
@@ -0,0 +1,24 @@
+#Lane & qtr level metrics
+SELECT from_city, 
+       to_city, 
+       from_cluster, 
+       to_cluster, 
+       qtr_name, 
+       COUNT(order_id)                         AS orders, 
+       COUNT(DISTINCT actual_sp_number)        AS sps, 
+       COUNT(DISTINCT registration_number)     AS trucks, 
+       ROUND(SUM(gmv), 0)                      AS gmv, 
+       ROUND(SUM(revenue), 0)                  AS revenue, 
+       ROUND(SUM(base_cost), 0)                AS base_cost, 
+       ROUND(SUM(cost), 0)                     AS cost, 
+       ROUND(SUM(tiger_tonnage), 0)            AS tiger_tonnage 
+FROM   zinka.table_name 
+WHERE  year >= 2017 
+       AND qtr <= 15 
+GROUP  BY 1, 
+          2, 
+          3, 
+          4, 
+          5, 
+          6, 
+          7 
