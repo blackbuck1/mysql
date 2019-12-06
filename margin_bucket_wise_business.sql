f0849eb14d50536236055e1f86739a222155f269
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2019-12-06 19:09:26 +0530

    Update margin_bucket_wise_business.sql

--- margin_bucket_wise_business.sql
+++ margin_bucket_wise_business.sql
@@ -1,35 +1,28 @@
 SELECT 
-  CASE WHEN ngmp <= -0.05 THEN '-5% and below' WHEN ngmp > -0.05 
-  AND ngmp <= 0 THEN '-5% to 0%' WHEN ngmp > 0 
-  AND ngmp <= 0.05 THEN '0% to 5%' ELSE '5% above' END AS gm_bucket, 
-  customer_classification, 
+  CASE WHEN ngmp < -0.1 THEN '-10% and below' 
+  WHEN ngmp >= -0.1  AND ngmp < 0 THEN '-10% to 0%' 
+  WHEN ngmp >= 0   AND ngmp < 0.1 THEN '0% to 10%' 
+  ELSE '10% above' END AS gm_bucket, 
   qtr_name, 
   SUM(gmv) AS gmv, 
-  SUM(revenue) AS revenue, 
-  SUM(NGM2) AS margin, 
-  COUNT(DISTINCT actual_customer_name) AS shipper_count 
+  SUM(revenue) AS revenue
 FROM 
   (
     SELECT 
       actual_customer_name, 
       customer_classification, 
       qtr_name, 
-      SUM(gmv) AS gmv, 
-      SUM(revenue) AS revenue, 
-      SUM(revenue - base_cost + bonus) AS NGM2, 
-      SUM(revenue - base_cost + bonus)/ SUM(gmv) AS ngmp 
+      gmv, 
+      revenue, 
+      #CASE WHEN customer_classification = 'Corporate' THEN revenue - base_cost + bonus ELSE revenue - base_cost END AS ngm2, 
+      (CASE WHEN customer_classification = 'Corporate' THEN revenue - base_cost + bonus ELSE revenue - base_cost END)/ gmv AS ngmp 
     FROM 
       zlog.order_details 
     WHERE 
-      qtr = 16 
-      AND customer_classification = 'Corporate' 
-      AND actual_customer_name <> '' 
-    GROUP BY 
-      1, 
-      2, 
-      3
+      qtr <= 16 
+      #AND customer_classification = 'Corporate' 
+      #AND actual_customer_name <> '' 
   ) a 
 GROUP BY 
   1, 
-  2, 
-  3
+  2
