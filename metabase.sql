e311c72404ef2aaeb65ef48d0bab488d74d1deac
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2020-01-09 10:54:47 +0530

    Update metabase.sql

--- metabase.sql
+++ metabase.sql
@@ -57,3 +57,23 @@ GROUP  BY from_city_id,
 SELECT truck_type, 
        rtkm_tonnage AS capacity 
 FROM   base_trucktype 
+
+#Order Id to Truck & SP Mapping
+
+select a.order_id,
+b.username,
+a.supply_partner_id,
+a.mobile_number,
+a.assigned_truck_id,
+a.registration_number,
+c.name
+#a.* 
+from base_ordersupplypartnertruckhistory a
+left join zinka.auth_user  b
+on a.supply_partner_id = b.id
+left join zinka.base_truck c
+on a.assigned_truck_id = c.id
+where order_id IN (
+'691550',
+'1220286'
+)
