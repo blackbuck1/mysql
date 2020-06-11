90f3990391fd2bd5a92bfa654e4ff5110919dc75
Nihal Singh Verma <37439986+nihalsinghverma@users.noreply.github.com>
2020-06-11 12:30:02 +0530

    Update metrics_mis.sql

--- metrics_mis.sql
+++ metrics_mis.sql
@@ -1,31 +1,75 @@
-# Revenue Bucket & Customer wise Customer Count  
-SELECT month, 
-       customer_classification, 
-       bucket, 
-       COUNT(DISTINCT actual_customer_name) AS Cust_Count 
-FROM   (SELECT *, 
-               CASE 
-                 WHEN revenue > 5000000 THEN '50+' 
-                 WHEN revenue > 3000000 THEN '30+' 
-                 WHEN revenue > 0 THEN '30-' 
-                 ELSE '' 
-               end AS Bucket 
-        FROM   (SELECT month, 
-                       actual_customer_name, 
-                       customer_classification, 
-                       ROUND(SUM(revenue), 0) AS revenue 
-                FROM   zinka.base_order_v8 
-                WHERE  month = 44 
-                       AND customer_type <> 'Delta' 
-                GROUP  BY month, 
-                          actual_customer_name, 
-                          customer_classification 
-                ORDER  BY revenue DESC) A) B 
-GROUP  BY month, 
-          customer_classification, 
-          bucket 
-ORDER  BY month, 
-          customer_classification, 
-          bucket; 
-          
-#  
+# Shipper Bucket
+
+SELECT
+   month,
+   business_type,
+   bucket,
+   COUNT(DISTINCT actual_customer_name) AS Cust_Count 
+FROM
+   (
+      SELECT
+         *,
+         CASE
+            WHEN
+               revenue > 5000000 
+            THEN
+               '50+' 
+            WHEN
+               revenue > 3000000 
+            THEN
+               '30+' 
+            WHEN
+               revenue > 0 
+            THEN
+               '30-' 
+            ELSE
+               '' 
+         end
+         AS Bucket 
+      FROM
+         (
+            SELECT
+               month,
+               actual_customer_name,
+               business_type,
+               ROUND(SUM(revenue), 0) AS revenue 
+            FROM
+               blackbuck_freight.orders 
+            WHERE
+               MONTH = 54 
+               AND customer_type <> 'Delta' 
+            GROUP BY
+               1,
+               2,
+               3 
+            ORDER BY
+               4 DESC
+         )
+         A
+   )
+   B 
+GROUP BY
+   1,
+   2,
+   3
+ORDER BY
+   1,
+   2,
+   3;
+   
+# Shipper Revenue Growth Trends - Overall
+
+SELECT
+   month,
+   customer_onboard_year,
+   ROUND(SUM(revenue), 0) AS revenue 
+FROM
+   blackbuck_freight.orders 
+WHERE
+   MONTH = 54 
+   AND customer_type <> 'Delta' 
+GROUP BY
+   1,
+   2 
+ORDER BY
+   2
